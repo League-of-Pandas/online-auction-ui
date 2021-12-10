d023e7a9dcde5c1573891c98903499698fa4cb5d
Tasneemalabsi <tasneem.alabsi@gmail.com>
2021-12-10 23:53:06 +0200

    feat: add static data to browse page

---
+++ component/Browse.js
@@ -0,0 +1,34 @@
+import Image from "next/image"
+
+export default function Browse() {
+    const src = 'https://images.unsplash.com/photo-1584650605009-b70344d8cd42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YW50aXF1ZSUyMGNhcnxlbnwwfHwwfHw%3D&w=1000&q=80';
+    const arr=[1,2,3,4,5,6,7,8,9,1,2,3]
+    return(
+        <>
+        <div className="px-4 mx-20">
+        <label htmlFor="cars">Filter</label>
+        <select name="cars" id="cars" className="px-4 mx-2">
+        <option value="cars">cars</option>
+        </select>
+        </div>
+        {arr.map((item,key)=>{
+            return(
+                <>
+        <div key={`${key}`} className="flex my-4"> 
+            <Image alt='whatever' loader={() => src} src={src} width={200} height={200}/>
+            <div className="px-4 py-8">
+            <h2 className='text-xl '>Old Vehicle</h2>
+            <h3>Bids:20 | current bid:100 $</h3>
+            <h3>california, USA</h3>
+            </div>
+        </div>
+        {/* <hr /> */}
+        </>
+            )
+        }
+        )
+        }
+        </>
+
+    )
+}
